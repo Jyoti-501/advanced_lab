9e097f59fbc46eff2715903c2308a04b77dd2811
Jyoti-510 <dollyind562@gmail.com>
2024-09-13 22:01:55 +0530

    question 1

--- question1.cpp
+++ question1.cpp
@@ -1,15 +1,26 @@
-// C++ program to display "Hello World"
- 
-// Header file for input output functions
 #include <iostream>
+#include <vector>
 using namespace std;
- 
-// Main() function: where the execution of
-// program begins
-int main()
-{
-    // Prints hello world
-    cout << "Hello World";
- 
+
+void moveZeroes(vector<int>& nums) {
+    int nonZeroIndex = 0; 
+    for (int i = 0; i < nums.size(); ++i) {
+        if (nums[i] != 0) {
+            nums[nonZeroIndex++] = nums[i];
+        }
+    }
+    for (int i = nonZeroIndex; i < nums.size(); ++i) {
+        nums[i] = 0;
+    }
+}
+
+int main() {
+    vector<int> nums = {0, 1, 0, 3, 12};
+    
+    moveZeroes(nums);
+    for (int num : nums) {
+        cout << num << " ";
+    }
+    
     return 0;
 }
